2d8a929039c01870b7618f9599c8f0fb881a81f1
ChrissyMac <11790059+chri55y@users.noreply.github.com>
2017-12-14 17:16:26 -0600

    create and code IdeasContainer component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import './App.css';
-import IdeasContainer from './components/IdeasContainer';
+import React, { Component } from 'react'
+import './App.css'
+import IdeasContainer from './components/IdeasContainer'
 
 class App extends Component {
   render() {
---
+++ src/components/IdeasContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class IdeasContainer extends React.Component {
+    render() {
+        return (
+            <div className="">
+                Ideas (woohoo)
+
+            </div>
+        );
+    }
+}
+
+export default IdeasContainer
