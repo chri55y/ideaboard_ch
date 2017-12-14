df74cb09b123be1ed95a6b64bd6b4b9eb0cda94d
ChrissyMac <11790059+chri55y@users.noreply.github.com>
2017-12-14 17:42:21 -0600

    adds constructor to IdeasContainer, including adding state

--- src/components/IdeasContainer.js
+++ src/components/IdeasContainer.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react'
 
 class IdeasContainer extends React.Component {
+    // need to give state to prepare for api
+    constructor(props) {
+        super(props)
+        this.state = {
+            ideas:  []
+        }
+    }
+
     render() {
         return (
             <div className="">
