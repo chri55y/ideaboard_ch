3dd154979a4bc72603833759577a66060884478b
ChrissyMac <11790059+chri55y@users.noreply.github.com>
2017-12-14 18:26:09 -0600

    axios logic, seeking response from api

--- src/components/IdeasContainer.js
+++ src/components/IdeasContainer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import axios from 'axios'   // axios lib
 
 class IdeasContainer extends React.Component {
     // need to give state to prepare for api
@@ -13,7 +14,12 @@ class IdeasContainer extends React.Component {
         // this is how we're going to get our ideas from/through api
 
         // use JS lib axios
-
+        axios.get('http://http://localhost:3000/api/v1/ideas.json')
+        .then(response => {
+            console.log(response)
+            this.setState({ ideas: response.data })
+        })
+        .catch(error => console.log(error))
     }
 
     render() {
