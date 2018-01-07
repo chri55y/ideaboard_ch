34082a18678527e97503c07f3b8f27939e4c1a76
ChrissyMac <11790059+chri55y@users.noreply.github.com>
2018-01-07 15:43:38 -0600

    create addNewIdea to handle button click. currently just posts / logs an idea with empty title and body

--- src/components/IdeasContainer.js
+++ src/components/IdeasContainer.js
@@ -14,18 +14,32 @@ class IdeasContainer extends React.Component {
     componentDidMount() {
         // this is how we're going to get our ideas from/through api
         axios.get('http://localhost:3001/api/v1/ideas.json')
-        .then(response => {
-            // console.log(response)
-            this.setState({ ideas: response.data })
-        })
-        .catch(error => console.log(error))
+            .then(response => {
+                // console.log(response)
+                this.setState({ ideas: response.data })
+            })
+            .catch(error => console.log(error))
+    }
+
+    // defining as ES6 arrow function allows us to avoid adding binds in constructor
+    addNewIdea = () => {
+        // copied from above, but changed the following
+        // get > post
+        // remove .json
+        // send
+        axios.post('http://localhost:3001/api/v1/ideas', {idea: {title: '', body: ''}})
+            .then(response => {
+                console.log(response)
+                // this.setState({ ideas: response.data })
+            })
+            .catch(error => console.log(error))
     }
 
     render() {
         return (
             <div>
                 <div>
-                    <button className="newIdeaButton">
+                    <button className="newIdeaButton" onClick={this.addNewIdea}>
                         Add New Idea
                     </button>
                 </div>
